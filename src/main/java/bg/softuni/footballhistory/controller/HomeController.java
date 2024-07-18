508361e9cb1f87d70e668801a0fddbef6fba53fa
AlexanderVelikov <alex.dimitrov.velikov9311@gmail.com>
2024-07-18 13:02:18 +0300

    html and css add

---
+++ src/main/java/bg/softuni/footballhistory/controller/HomeController.java
@@ -0,0 +1,13 @@
+package bg.softuni.footballhistory.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping()
+    public String getHome(){
+        return "profile";
+    }
+}
---
+++ src/test/java/bg/softuni/footballhistory/FootballHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.footballhistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballHistoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
